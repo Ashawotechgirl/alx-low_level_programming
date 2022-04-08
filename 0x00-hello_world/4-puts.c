145821ed5db45c63e9015931ea5ac39238cf00fa
Ashawotechgirl <adunnitiwatope@gmail.com>
2022-04-07 17:55:59 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 afterprinting the function
+ */
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle"):
+		return (0);
