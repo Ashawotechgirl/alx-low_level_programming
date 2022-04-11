67d3a6581708d02f283c8df520b8998eff24a8f4
Adunni Tiwatope <adunnitiwatope@gmail.com>
2022-04-11 17:40:18 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; C++)
+	{
+		putchar(c);
+	}
+	
+	putchart('\n');
+
+	return(0);
+}	
