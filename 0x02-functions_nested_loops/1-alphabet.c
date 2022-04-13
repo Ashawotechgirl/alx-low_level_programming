c2dbff7b7d2ae51e41863806184d1b354fd3dd28
Adunni Tiwatope <adunnitiwatope@gmail.com>
2022-04-13 19:51:55 +0000

    1-alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet low
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z';
+{
+_putchar(alphabet);
+}
+
+_putchar(alphabet);
+
+}
