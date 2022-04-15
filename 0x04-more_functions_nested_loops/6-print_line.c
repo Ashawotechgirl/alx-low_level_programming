0ebe50fd8e1e6dc9c03a8fe37bf82de880b841e5
Adunni Tiwatope <adunnitiwatope@gmail.com>
2022-04-15 16:22:18 +0000

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line is printed.
+ * @n: times staright line is printed.
+ * Return: no return
+ */
+void print_lines(int n)
+{
+	int i = 0
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
