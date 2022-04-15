24c71a987bfd95c87ee61188fa6b4ea25e0927d0
Adunni Tiwatope <adunnitiwatope@gmail.com>
2022-04-15 15:23:51 +0000

    task 3

--- 0x04-more_functions_nested_loops/3-print_number.c
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -5,14 +5,13 @@
  *
  * Return: void
  */
-
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; c <= 10; i++)
 	{
-		_putchar(c);
+		_putchar('0' + i);
 	}
 	_putchar('\n');
 }
