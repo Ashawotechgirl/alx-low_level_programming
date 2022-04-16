3079a4b022059c4e4967ed86ae3ff3f4b26eede9
Adunni Tiwatope <adunnitiwatope@gmail.com>
2022-04-16 22:34:25 +0000

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,7 @@
 /**
  *_isupper - checks if letters are uppercase.
  *@c: input.
- *
- *Return: 1 if c is uppercase
- *0 otherwise.
+ *Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
